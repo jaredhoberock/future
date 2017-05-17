41a00b753a47cef2c794b1f672de68febfcb042a
Jared Hoberock <jaredhoberock@gmail.com>
2017-05-17 15:41:50 -0500

    Implement make_ready_future and make_exceptional_future and tests

--- test_future.cpp
+++ test_future.cpp
@@ -10,63 +10,125 @@ int main()
   }
 
   {
-    // move construction
-    std::experimental::future<int> f0 = std::experimental::promise<int>().get_future();
-    assert(f0.valid());
+    // from promise
 
-    std::experimental::future<int> f1 = std::move(f0);
-    assert(!f0.valid());
-    assert(f1.valid());
-  }
-
-  {
-    // move assignment
-    std::experimental::future<int> f1 = std::experimental::promise<int>().get_future();
-    assert(f1.valid());
-
-    std::experimental::future<int> f2;
-    assert(!f2.valid());
-
-    f2 = std::move(f1);
-    assert(!f1.valid());
-    assert(f2.valid());
-  }
-
-  {
-    // get value
-    std::promise<int> p;
-
-    std::future<int> f = p.get_future();
-    assert(f.valid());
-
-    p.set_value(13);
-
-    int result = f.get();
-    assert(result == 13);
-    assert(!f.valid());
-  }
-
-  {
-    // get exception
-    std::promise<int> p;
-
-    std::future<int> f = p.get_future();
-    assert(f.valid());
-
-    p.set_exception(std::make_exception_ptr(std::runtime_error("error")));
-
-    try
     {
+      // move construction
+      std::experimental::future<int> f0 = std::experimental::promise<int>().get_future();
+      assert(f0.valid());
+
+      std::experimental::future<int> f1 = std::move(f0);
+      assert(!f0.valid());
+      assert(f1.valid());
+    }
+
+    {
+      // move assignment
+      std::experimental::future<int> f1 = std::experimental::promise<int>().get_future();
+      assert(f1.valid());
+
+      std::experimental::future<int> f2;
+      assert(!f2.valid());
+
+      f2 = std::move(f1);
+      assert(!f1.valid());
+      assert(f2.valid());
+    }
+
+    {
+      // get value
+      std::experimental::promise<int> p;
+
+      std::experimental::future<int> f = p.get_future();
+      assert(f.valid());
+
+      p.set_value(13);
+
       int result = f.get();
-      assert(0);
+      assert(result == 13);
+      assert(!f.valid());
     }
-    catch(std::runtime_error e)
+
     {
-      assert(std::string(e.what()) == "error");
+      // get exception
+      std::experimental::promise<int> p;
+
+      std::experimental::future<int> f = p.get_future();
+      assert(f.valid());
+
+      p.set_exception(std::make_exception_ptr(std::runtime_error("error")));
+
+      try
+      {
+        int result = f.get();
+        assert(0);
+      }
+      catch(std::runtime_error e)
+      {
+        assert(std::string(e.what()) == "error");
+      }
+      catch(...)
+      {
+        assert(0);
+      }
     }
-    catch(...)
+  }
+
+
+  {
+    // from make_ready_future
+
     {
-      assert(0);
+      // move construction
+      std::experimental::future<int> f0 = std::experimental::make_ready_future<int>(13);
+      assert(f0.valid());
+
+      std::experimental::future<int> f1 = std::move(f0);
+      assert(!f0.valid());
+      assert(f1.valid());
+    }
+
+    {
+      // move assignment
+      std::experimental::future<int> f1 = std::experimental::make_ready_future<int>(13);
+      assert(f1.valid());
+
+      std::experimental::future<int> f2;
+      assert(!f2.valid());
+
+      f2 = std::move(f1);
+      assert(!f1.valid());
+      assert(f2.valid());
+    }
+
+    {
+      // get value
+      std::experimental::future<int> f = std::experimental::make_ready_future<int>(13);
+      assert(f.valid());
+
+      int result = f.get();
+      assert(result == 13);
+      assert(!f.valid());
+    }
+
+    {
+      // get exception
+      std::experimental::future<int> f = std::experimental::make_exceptional_future<int>(std::runtime_error("error"));
+      assert(f.valid());
+
+      try
+      {
+        int result = f.get();
+        assert(0);
+      }
+      catch(std::runtime_error e)
+      {
+        assert(std::string(e.what()) == "error");
+      }
+      catch(...)
+      {
+        assert(0);
+      }
     }
   }
 
