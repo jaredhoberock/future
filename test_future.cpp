8e3a099da6fff0e2d79c5fc35da69c0e39c3bfd9
Jared Hoberock <jaredhoberock@gmail.com>
2017-05-17 14:19:23 -0500

    Fix async_future move assignment and test

--- test_future.cpp
+++ test_future.cpp
@@ -9,6 +9,29 @@ int main()
     assert(!f0.valid());
   }
 
+  {
+    // move construction
+    std::experimental::future<int> f0 = std::experimental::promise<int>().get_future();
+    assert(f0.valid());
+
+    std::experimental::future<int> f1 = std::move(f0);
+    assert(!f0.valid());
+    assert(f1.valid());
+  }
+
+  {
+    // move assignment
+    std::experimental::future<int> f1 = std::experimental::promise<int>().get_future();
+    assert(f1.valid());
+
+    std::experimental::future<int> f2;
+    assert(!f2.valid());
+
+    f2 = std::move(f1);
+    assert(!f1.valid());
+    assert(f2.valid());
+  }
+
   std::cout << "OK" << std::endl;
 }
 
