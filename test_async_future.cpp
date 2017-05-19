0a24e2cfdd5bc1809184dabf12c00ed0b74f930c
Jared Hoberock <jaredhoberock@gmail.com>
2017-05-19 16:49:42 -0500

    Add initial implementation and test of async_future::then()

--- test_async_future.cpp
+++ test_async_future.cpp
@@ -1,18 +1,60 @@
 #include <experimental/future>
 #include <cassert>
 
+struct has_execute_member
+{
+  const has_execute_member& context() const noexcept
+  {
+    return *this;
+  }
+
+  template<class Function>
+  void execute(Function&& f) const
+  {
+    f();
+  }
+
+  bool operator==(const has_execute_member&) const noexcept
+  {
+    return true;
+  }
+
+  bool operator!=(const has_execute_member&) const noexcept
+  {
+    return false;
+  }
+};
+
 int main()
 {
   using namespace std::experimental::v1::detail;
 
   {
-    // test make_ready()
+    // make_ready()
 
     async_future<int> f0 = async_future<int>::make_ready(13);
     assert(f0.valid());
     assert(f0.is_ready());
 
     assert(13 == f0.get());
+  }
+
+  {
+    // then()
+
+    async_future<int> f0 = async_future<int>::make_ready(13);
+    assert(f0.valid());
+    assert(f0.is_ready());
+
+    has_execute_member exec;
+    auto f1 = f0.then(exec, [](async_future<int> predecessor)
+    {
+      return predecessor.get() + 7;
+    });
+
+    int result = f1.get();
+
+    assert(result == 20);
   }
 
   std::cout << "OK" << std::endl;
