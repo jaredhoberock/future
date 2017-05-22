f286f19714e54ed71b6e811c447405c4e04bcc12
Jared Hoberock <jaredhoberock@gmail.com>
2017-05-22 17:25:35 -0500

    Test void futures

--- test_future.cpp
+++ test_future.cpp
@@ -4,8 +4,14 @@
 int main()
 {
   {
-    // default construction
+    // non-void default construction
     std::experimental::future<int> f0;
+    assert(!f0.valid());
+  }
+
+  {
+    // void default construction
+    std::experimental::future<void> f0;
     assert(!f0.valid());
   }
 
@@ -13,7 +19,7 @@ int main()
     // from promise
 
     {
-      // move construction
+      // non-void move construction
       std::experimental::future<int> f0 = std::experimental::promise<int>().get_future();
       assert(f0.valid());
 
@@ -23,7 +29,17 @@ int main()
     }
 
     {
-      // move assignment
+      // void move construction
+      std::experimental::future<void> f0 = std::experimental::promise<void>().get_future();
+      assert(f0.valid());
+
+      std::experimental::future<void> f1 = std::move(f0);
+      assert(!f0.valid());
+      assert(f1.valid());
+    }
+
+    {
+      // non-void move assignment
       std::experimental::future<int> f1 = std::experimental::promise<int>().get_future();
       assert(f1.valid());
 
@@ -36,7 +52,20 @@ int main()
     }
 
     {
-      // get value
+      // void move assignment
+      std::experimental::future<void> f1 = std::experimental::promise<void>().get_future();
+      assert(f1.valid());
+
+      std::experimental::future<void> f2;
+      assert(!f2.valid());
+
+      f2 = std::move(f1);
+      assert(!f1.valid());
+      assert(f2.valid());
+    }
+
+    {
+      // non-void get
       std::experimental::promise<int> p;
 
       std::experimental::future<int> f = p.get_future();
@@ -50,7 +79,20 @@ int main()
     }
 
     {
-      // get exception
+      // void get
+      std::experimental::promise<void> p;
+
+      std::experimental::future<void> f = p.get_future();
+      assert(f.valid());
+
+      p.set_value();
+
+      f.get();
+      assert(!f.valid());
+    }
+
+    {
+      // non-void get exception
       std::experimental::promise<int> p;
 
       std::experimental::future<int> f = p.get_future();
@@ -72,6 +114,30 @@ int main()
         assert(0);
       }
     }
+
+    {
+      // void get exception
+      std::experimental::promise<void> p;
+
+      std::experimental::future<void> f = p.get_future();
+      assert(f.valid());
+
+      p.set_exception(std::make_exception_ptr(std::runtime_error("error")));
+
+      try
+      {
+        f.get();
+        assert(0);
+      }
+      catch(std::runtime_error e)
+      {
+        assert(std::string(e.what()) == "error");
+      }
+      catch(...)
+      {
+        assert(0);
+      }
+    }
   }
 
 
@@ -79,7 +145,7 @@ int main()
     // from make_ready_future
 
     {
-      // move construction
+      // non-void move construction
       std::experimental::future<int> f0 = std::experimental::make_ready_future<int>(13);
       assert(f0.valid());
 
@@ -89,7 +155,17 @@ int main()
     }
 
     {
-      // move assignment
+      // void move construction
+      std::experimental::future<void> f0 = std::experimental::make_ready_future();
+      assert(f0.valid());
+
+      std::experimental::future<void> f1 = std::move(f0);
+      assert(!f0.valid());
+      assert(f1.valid());
+    }
+
+    {
+      // non-void move assignment
       std::experimental::future<int> f1 = std::experimental::make_ready_future<int>(13);
       assert(f1.valid());
 
@@ -102,7 +178,20 @@ int main()
     }
 
     {
-      // get value
+      // void move assignment
+      std::experimental::future<void> f1 = std::experimental::make_ready_future();
+      assert(f1.valid());
+
+      std::experimental::future<void> f2;
+      assert(!f2.valid());
+
+      f2 = std::move(f1);
+      assert(!f1.valid());
+      assert(f2.valid());
+    }
+
+    {
+      // non-void get
       std::experimental::future<int> f = std::experimental::make_ready_future<int>(13);
       assert(f.valid());
 
@@ -112,13 +201,42 @@ int main()
     }
 
     {
-      // get exception
+      // void get
+      std::experimental::future<void> f = std::experimental::make_ready_future();
+      assert(f.valid());
+
+      f.get();
+      assert(!f.valid());
+    }
+
+    {
+      // non-void get exception
       std::experimental::future<int> f = std::experimental::make_exceptional_future<int>(std::runtime_error("error"));
       assert(f.valid());
 
       try
       {
         int result = f.get();
+        assert(0);
+      }
+      catch(std::runtime_error e)
+      {
+        assert(std::string(e.what()) == "error");
+      }
+      catch(...)
+      {
+        assert(0);
+      }
+    }
+
+    {
+      // void get exception
+      std::experimental::future<void> f = std::experimental::make_exceptional_future<void>(std::runtime_error("error"));
+      assert(f.valid());
+
+      try
+      {
+        f.get();
         assert(0);
       }
       catch(std::runtime_error e)
