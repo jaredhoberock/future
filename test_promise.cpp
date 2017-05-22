1ad113e217ccb5adb5b12c6725c7931c67114971
Jared Hoberock <jaredhoberock@gmail.com>
2017-05-22 17:03:27 -0500

    Test promise<void>

--- test_promise.cpp
+++ test_promise.cpp
@@ -5,7 +5,7 @@
 void test()
 {
   {
-    // test get_future
+    // test non-void get_future
     std::experimental::promise<int> p;
 
     std::experimental::future<int> f = p.get_future();
@@ -28,7 +28,30 @@ void test()
   }
 
   {
-    // test set_value() / .get()
+    // test void get_future
+    std::experimental::promise<void> p;
+
+    std::experimental::future<void> f = p.get_future();
+
+    assert(f.valid());
+
+    try
+    {
+      std::experimental::future<void> f = p.get_future();
+      assert(0);
+    }
+    catch(std::future_error e)
+    {
+      assert(e.code() == std::future_errc::future_already_retrieved);
+    }
+    catch(...)
+    {
+      assert(0);
+    }
+  }
+
+  {
+    // test non-void set_value() / .get()
     std::experimental::promise<int> p;
 
     int expected_result = 13;
@@ -40,7 +63,16 @@ void test()
   }
 
   {
-    // test superfluous set_value()
+    // test void set_value() / .get();
+    std::experimental::promise<void> p;
+
+    p.set_value();
+
+    p.get_future().get();
+  }
+
+  {
+    // test non-void superfluous set_value()
     std::experimental::promise<int> p;
 
     p.set_value(13);
@@ -49,6 +81,28 @@ void test()
     {
       // calling set_value() againt should throw promise_already_satisfied
       p.set_value(13);
+      assert(0);
+    }
+    catch(std::future_error e)
+    {
+      assert(e.code() == std::future_errc::promise_already_satisfied);
+    }
+    catch(...)
+    {
+      assert(0);
+    }
+  }
+
+  {
+    // test void superfluous set_value()
+    std::experimental::promise<void> p;
+
+    p.set_value();
+
+    try
+    {
+      // calling set_value() againt should throw promise_already_satisfied
+      p.set_value();
       assert(0);
     }
     catch(std::future_error e)
